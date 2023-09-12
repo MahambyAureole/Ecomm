f4c70fa260e8d87ca059354078f988a9285f9493
MahambyAureole <rasolonantenaim43@gmail.com>
2023-09-13 02:32:41 +0300

    Mise à jour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service-back.service.ts
+++ src/app/service-back.service.ts
@@ -11,15 +11,29 @@ export class ServiceBackService {
   
   constructor(private http: HttpClient) { }
   
+  // CONNEXION AFFICHAGE ENTRE PRODUIT BACK ET FRONT)
   afficherUtilisateur(): Observable<any> {
     return this.http.get(`${this.apiUrl}/afficheUtil`);
   }
 
+  // CONNEXION AJOUT ENTRE PRODUIT BACK ET FRONT)
   ajoutProduit(produit: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/ajouterProduit`, produit);
   }
 
+  // CONNEXION LISTE ENTRE PRODUIT BACK ET FRONT)
   listeProduit(): Observable<any>{
     return this.http.get<any>(`${this.apiUrl}/listeProduit`);
   }
+
+  // CONNEXION MODIFIER ENTRE PRODUIT BACK ET FRONT)
+  modifierProduit( idProd: number,produit: any): Observable<any>{
+    return this.http.patch<any>(`${this.apiUrl}/modifierProd/${idProd}`, produit);
+  }
+
+  // CONNEXION SUPPRIMER ENTRE PRODUIT BACK ET FRONT)
+  supprimerProduit(idProd: number):Observable<Object>{
+    return this.http.delete(`${this.apiUrl}/effaceProd/${idProd}`);
+  }
+
 }
